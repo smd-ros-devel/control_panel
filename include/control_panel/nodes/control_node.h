0cea8a360f7da6d74be72df6b81bb131150cf2d7
1925123 <1925123@355df293-e355-4a6a-999b-acf4fd1662f5>
2012-02-12 05:41:55 +0000

    Added license to all node header files

--- include/control_panel/nodes/control_node.h
+++ include/control_panel/nodes/control_node.h
@@ -1,13 +1,37 @@
-/******************************************************************************
- * control_node.h
+/*
+ * Copyright (c) 2011, 2012 SDSM&T RIAS.
+ * All rights reserved.
  *
- * Author:      Matt Richard
- * Date:        Aug 31, 2011
- * Description: ROS node for controlling a robot's movement. The node receives
- * 				a command (keyboard or joystick) and translates it into linear
- *				and angular x, y, and z velocity.
- *****************************************************************************/
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
+
+/**
+ * \file   control_node.h
+ * \date   Aug 31, 2011
+ * \author Matt Richard
+ */
 #ifndef CONTROL_PANEL_CONTROL_NODE_H
 #define CONTROL_PANEL_CONTROL_NODE_H
 
@@ -18,7 +42,10 @@
 #include <string>
 #include "control_panel/globals.h"
 
-
+/**
+ * \class ControlNode
+ * \brief ROS node for controlling a robot's movement
+ */
 class ControlNode : public QObject
 {
 	Q_OBJECT
--- include/control_panel/nodes/odometry_node.h
+++ include/control_panel/nodes/odometry_node.h
@@ -1,11 +1,37 @@
-/******************************************************************************
- * odometry_node.h
+/*
+ * Copyright (c) 2011, 2012 SDSM&T RIAS.
+ * All rights reserved.
  *
- * Author:      Matt Richard
- * Date:        Aug 12, 2011
- * Description: ROS node for receiving odometry data.
- *****************************************************************************/
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
+
+/**
+ * \file   odometry_node.h
+ * \date   Aug 12, 2011
+ * \author Matt Richard
+ */
 #ifndef CONTROL_PANEL_ODOMETRY_NODE_H
 #define CONTROL_PANEL_ODOMETRY_NODE_H
 
@@ -14,11 +40,13 @@
 #include <QQuaternion>
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
-#include <stdio.h>
 #include <string>
 #include "control_panel/globals.h"
 
-
+/**
+ * \class OdometryNode
+ * \brief ROS node for receving a robot's odometry data
+ */
 class OdometryNode : public QObject
 {
 	Q_OBJECT
