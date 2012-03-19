0f725f34e902f157bcdb77de0ed45cfc3c789ab8
1925123 <1925123@355df293-e355-4a6a-999b-acf4fd1662f5>
2012-03-19 03:40:45 +0000

    Added license to all files missing the license.

--- include/control_panel/widgets/odometry_display.h
+++ include/control_panel/widgets/odometry_display.h
@@ -1,12 +1,37 @@
-/**
- * odometry_display.h
+/*
+ * Copyright (c) 2011, 2012 SDSM&T RIAS.
+ * All rights reserved.
  *
- * Author:      Matt Richard
- * Date:        Jan 26, 2012
- * Description: Displays odometry information (position, orientation,
- *              linear velocity, and angular velocity).
- **/
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
+
+/**
+ * \file   odometry_display.h
+ * \date   Jan 26,2012
+ * \author Matt Richard
+ */
 #ifndef CONTROL_PANEL_ODOMETRY_DISPLAY_H
 #define CONTROL_PANEL_ODOMETRY_DISPLAY_H
 
@@ -24,6 +49,11 @@ QT_END_NAMESPACE
 #include "heading_indicator.h"
 #include "control_panel/globals.h"
 
+
+/**
+ * \class OdometryDisplay
+ * \brief Displays odometry information (position, orientation, linear velocity, and angular velocity).
+ */
 class OdometryDisplay : public QWidget
 {
     Q_OBJECT
